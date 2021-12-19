c759f9248c46e21ad2a41e667e77f32306e37420
GrahamWaggener <44220223+GrahamWaggener@users.noreply.github.com>
2021-12-19 15:38:00 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import plotly.express as px
-import pandas as pd
+import plotly.express as px #For interactive map functionality
+import pandas as pd #For reading the CSV file
 
 if __name__ == '__main__':
-    df = pd.read_csv("routes2.csv")
+    df = pd.read_csv("routes2.csv") #Creates a dataframe of the climbing routes from mountainproject.com
     states = list()
     for i in range(len(df['Location'])):
         state = df['Location'][i].split('>')
         states.append(state[-1])
-    df['states'] = states
-    df['displays'] = df['Route'] + ' , ' + df['Rating'] + ' , ' + df['states']
-    fig = px.scatter_geo(df,lat='Lat',lon='Lon', hover_name='displays')
+    df['states'] = states #Adds a new column of the state to the dataframe of the last item in the location data
+    df['displays'] = df['Route'] + ' , ' + df['Rating'] + ' , ' + df['states'] #Combining the route name, rating, and state
+    fig = px.scatter_geo(df,lat='Lat',lon='Lon', hover_name='displays') #Plots the locations on a map with data when hovered over
     fig.update_layout(title = 'Climbing Routes', title_x=0.5)
     fig.show()
