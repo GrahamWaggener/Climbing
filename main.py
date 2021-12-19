74aac0fdeb071e40161e1cd7b702698e70581565
GrahamWaggener <44220223+GrahamWaggener@users.noreply.github.com>
2021-12-19 15:15:04 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import plotly.express as px
 import pandas as pd
 
 if __name__ == '__main__':
-    df = pd.read_csv("r2.csv")
+    df = pd.read_csv("routes2.csv")
     states = list()
     for i in range(len(df['Location'])):
         state = df['Location'][i].split('>')
@@ -10,5 +10,5 @@ if __name__ == '__main__':
     df['states'] = states
     df['displays'] = df['Route'] + ' , ' + df['Rating'] + ' , ' + df['states']
     fig = px.scatter_geo(df,lat='Lat',lon='Lon', hover_name='displays')
-    fig.update_layout(title = 'World map', title_x=0.5)
+    fig.update_layout(title = 'Climbing Routes', title_x=0.5)
     fig.show()
